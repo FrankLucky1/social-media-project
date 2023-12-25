57371727e0678bf6f211b48c5bedeb5004ba2e79
FrankLucky1 <franklucky0@gmail.com>
2023-12-25 15:17:16 +0100

    finish

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -152,7 +152,7 @@ export default function Home() {
                   <FiUploadCloud className="text-6xl" />
                   click the box to upload your party experience
                   <p className="text-sm text-gray-400">
-                    upload size should not be more that 3mb*
+                    upload size should not be more that 15mb*
                   </p>
                 </label>
 
@@ -168,7 +168,7 @@ export default function Home() {
                 />
               </div>
               <button className="bg-gradient-to-br from-purple-500 via-purple-400 to-violet-400 text-white p-2 rounded-lg">
-                {loading ? "Uploading..." : "Upload your images"}
+                {loading ? "Uploading..." : "Upload"}
               </button>
             </div>
           </div>
